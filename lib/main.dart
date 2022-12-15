2faac2320875c9702855b0980a1dd8635b1ef072
Admin <makwanadharmishtha44@gmail.com>
2022-12-15 20:10:38 +0530

    design2

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:myapp2/homePage.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
